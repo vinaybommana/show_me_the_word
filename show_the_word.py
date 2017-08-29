0f2b97633c44e62adaa92ccaffd364216e32385f
vinay bommana <vinaybommana7@gmail.com>
2017-08-29 20:41:00 +0530

    made a few changes to the script regarding the debug_print and only word without meaning words

--- show_the_word.py
+++ show_the_word.py
@@ -13,12 +13,14 @@ def show_word(word) :
     time.sleep(40)
     # print(word)
     words_list = word.split("-")
-    print(words_list)
+    # print(words_list)
     base_word = words_list[0].strip("* ")
-    print(base_word)
+    # print(base_word)
     if len(words_list) > 1 : 
         meaning = words_list[1].strip("\n")
         os.system('notify-send "{}" "{}"'.format(base_word, meaning)) # this should be the best line of the entire program
+    else :
+        os.system('notify-send "{}"'.format(base_word))
 
 def main() :
     while True :
