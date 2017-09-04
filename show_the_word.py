ac8e618ccad90508b6bbb74293e4e23d7a61c4c1
vinaybommana7 <vinaybommana7@gmail.com>
2017-09-04 22:08:46 +0530

    changed according to flake8 issues

--- show_the_word.py
+++ show_the_word.py
@@ -2,7 +2,7 @@ import os
 import time
 import fileinput
 import numpy as np
-import random
+# import random
 
 words = []
 
@@ -10,23 +10,26 @@ with fileinput.input(files=('vocabulary_words.md')) as f:
     for line in f:
         words.append(line)
 
-def show_word(word) :
+
+def show_word(word):
     time.sleep(60)
     # print(word)
     words_list = word.split("-")
     # print(words_list)
     base_word = words_list[0].strip("* ")
     # print(base_word)
-    if len(words_list) > 1 : 
+    if len(words_list) > 1:
         meaning = words_list[1].strip("\n")
-        os.system('notify-send "{}" "{}"'.format(base_word, meaning)) # this should be the best line of the entire program
-    else :
+        os.system('notify-send "{}" "{}"'.format(base_word, meaning))
+        # this should be the best line of the entire program
+    else:
         os.system('notify-send "{}"'.format(base_word))
 
-def main() :
-    while True :
+
+def main():
+    while True:
         show_word(np.random.choice(words))
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
